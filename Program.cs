0248d99e96842b1b8937040573f1849b29f5f8d9
SatoShinta <kanten3005@gmail.com>
2024-05-28 19:47:27 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+class Program
+{
+    static void Main()
+    {
+        int[] XYZ = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int X = XYZ[0];
+        int Y = XYZ[1];
+        int Z = XYZ[2];
+        if (Z >= 10)
+        {
+            Console.WriteLine("YES");
+        }
+        else if (X >= 10 && Y >= 10)
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
+        }
+    }
+}
